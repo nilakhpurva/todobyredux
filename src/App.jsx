b22344352f72408f22e4f09d93f9b330ce6f782c
nilakhpurva <163637517+nilakhpurva@users.noreply.github.com>
2024-12-29 18:20:33 +0530

    upmclasa

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { TodoRedux } from './components/TodoRedux';
-
-
- function App(){
-return(
-    <routes>
-         <Route path='/TodoRedux'element ={<TodoRedux/>}></Route>
-         <Route path='/Todo-Action'element ={<Todo-Action/>}></Route>
-    </routes>   
-)
- }
-
-export default App;
+import React from 'react'
 
 const App = () => {
-    return(
-        <div>
-            <todoApp/>
+  return (
+    <>
+    <div className='main-div'>
+        <div className='child-div'>
+            <figure>
+                <figcaption>Add your list here</figcaption>
+            </figure>
+            <div className='addItems'>
+                <input type="text" placeholder='Add Items...'/>
+                <i className='fa fa-plus add-btn'></i>
+            </div>
         </div>
-    );
-};
-// export default App;
+    </div>
+    
+    </>
+  )
+}
+export default App;
+
