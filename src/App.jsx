c236115465dbec52c8c3bf3f187df6c0de7c2426
nilakhpurva <163637517+nilakhpurva@users.noreply.github.com>
2024-12-29 18:09:30 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { TodoRedux } from './components/TodoRedux';
 
-function App() {
- return()
-}
+
+ function App(){
+return(
+    <routes>
+         <Route path='/TodoRedux'element ={<TodoRedux/>}></Route>
+         <Route path='/Todo-Action'element ={<Todo-Action/>}></Route>
+    </routes>   
+)
+ }
+
 export default App;
+
+const App = () => {
+    return(
+        <div>
+            <todoApp/>
+        </div>
+    );
+};
+// export default App;
