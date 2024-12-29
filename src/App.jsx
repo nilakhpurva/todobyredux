15bcc9096ae45d3bcabcc45e841ae14a8f004b95
nilakhpurva <163637517+nilakhpurva@users.noreply.github.com>
2024-12-29 18:27:18 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
+import Todo from "./components/Todo";
 
 const App = () => {
   return (
     <>
-    <div className='main-div'>
-        <div className='child-div'>
-            <figure>
-                <figcaption>Add your list here</figcaption>
-            </figure>
-            <div className='addItems'>
-                <input type="text" placeholder='Add Items...'/>
-                <i className='fa fa-plus add-btn'></i>
-            </div>
-        </div>
-    </div>
-    
+    <Todo/>
     </>
   )
 }
+  
 export default App;
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import "./todo.css";
 
-export const Todo = () => {
+const Todo = () => {
   return (
-    <div>Todo</div>
+    <>
+    <div className='main-div'>
+        <div className='child-div'>
+            <figure>
+                <figcaption>Add your list here</figcaption>
+            </figure>
+            <div className='addItems'>
+                <input type="text" placeholder='Add Items...'/>
+                <i className='fa fa-plus add-btn'></i>
+            </div>
+        </div>
+    </div>
+    
+    </>
   )
 }
+export default Todo;
+  
+
