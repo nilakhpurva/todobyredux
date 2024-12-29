ef8d7d7dc4202d444226527fe2328a27aef1fbd7
nilakhpurva <163637517+nilakhpurva@users.noreply.github.com>
2024-12-29 19:31:31 +0530

    updates

---
+++ src/components/reducer.js
@@ -0,0 +1,37 @@
+// reducer.js
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from './Actions';
+
+const initialState = {
+  todos: [],
+};
+
+let nextId = 1;
+
+export const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          { id: nextId++, text: action.payload, completed: false },
+        ],
+      };
+    case REMOVE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+    default:
+      return state;
+  }
+};
